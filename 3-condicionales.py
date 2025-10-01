7b9c2608e4d4f796f91bc29988f381eabcd5092c
c00rioslum <xlmriosx@gmail.com>
2025-10-01 17:09:07 -0300

    3

---
+++ 3-condicionales.py
@@ -0,0 +1,13 @@
+# CONTEXTO: EXISTEN SOLO 2 COLORES
+color_1 = 'negro'
+color_2 = 'negro'
+color_3 = 'negro'
+
+if (color_1 == color_2):
+    print("Color 1 y 2 son iguales")
+elif (color_1 == color_3):
+    print("Color 1 y 3 son iguales")
+elif (color_2 == color_3):
+    print("Color 2 y 3 son iguales")
+else:
+    print("Ninguno de los colores son iguales")
