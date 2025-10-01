00be6832f329a84c8cdf61906f2c2f637d94ee56
c00rioslum <xlmriosx@gmail.com>
2025-10-01 17:09:18 -0300

    3

---
+++ 4-while.py
@@ -0,0 +1,8 @@
+contador = 0
+lim_max = 3
+
+while (contador != lim_max + 1):
+    print('El contador va por la iteracion', contador)
+    contador += 1 # contador = contador + 1
+
+print('Salimos de la iteracion')
