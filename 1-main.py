4859490e2458c5ea46712cf66031cae88c38d170
c00rioslum <xlmriosx@gmail.com>
2025-10-01 17:03:08 -0300

    initial commit pepito

---
+++ 1-main.py
@@ -0,0 +1,67 @@
+# CTRL + S
+# Comentar: Selecciono, CTRL + K + C
+# Descomentar: Selecciono, CTRL + K + U
+
+# print('Hola mundo')
+
+# var_string = "esto es un string"
+# print(type(var_string))
+
+# var_integer = 101
+# print(var_integer, "su tipo de dato es:", type(var_integer))
+
+# var_float = 3.141521
+# print(var_float, "Su tipo de dato es:", type(var_float))
+
+# var_bool = True
+# print(var_bool, "Su tipo de dato es:", type(var_bool))
+
+# 0 AND 0 --> 0
+# 0 AND 1 --> 0
+# 1 AND 0 --> 0
+# 1 AND 1 --> 1
+
+# 0 OR 0 --> 0 
+# 0 OR 1 --> 1
+# 1 OR 0 --> 1
+# 1 OR 1 --> 1
+
+# 0 ORX 0 --> 0
+# 0 ORX 1 --> 1
+# 1 ORX 0 --> 1
+# 1 ORX 1 --> 0
+
+# 1 == 1 --> 1
+# 0 == 1 --> 0
+
+# 2 <> 0 --> 1
+# 2 <> 2 --> 0
+
+# 2 > 0 --> 1
+# 2 > 2 --> 0
+# 2 > 3 --> 0
+
+# 2 >= 0 --> 1
+# 2 >= 2 --> 1
+# 2 >= 3 --> 0
+
+# 2 < 0 --> 0
+# 2 < 2 --> 0
+# 2 < 3 --> 1
+
+# 2 <= 0 --> 0
+# 2 <= 2 --> 1
+# 2 <= 3 --> 1
+
+# Negado
+# not(0 ORX 0) --> 1
+# not(0 ORX 1 --> 0
+# not(1 ORX 0 --> 0
+# not(1 ORX 1 --> 1
+
+# not(1 == 1 --> 0
+# not(0 == 1 --> 1
+
+# not(2 <> 0 --> 0
+# not(2 <> 2 --> 1
+
